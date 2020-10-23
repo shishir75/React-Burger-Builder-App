dba057d6953c741ccddc575c0d80a3055204cf14
shishir75 <shishir.srdr16@gmail.com>
2020-10-24 03:13:48 +0600

    Adding a Dynamic Ingrediant Component

---
+++ src/components/Burger/BurgerIngrediant/BurgerIngrediant.js
@@ -0,0 +1,44 @@
+import React from "react";
+import classes from "./BurgerIngrediant.module.css";
+
+const BurgerIngrediant = (props) => {
+    let ingrediant = null;
+
+    switch (props.type) {
+        case "bread-buttom":
+            ingrediant = <div className={classes.BreadBottom}></div>;
+            break;
+
+        case "bread-top":
+            ingrediant = (
+                <div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>
+            );
+            break;
+
+        case "meat":
+            ingrediant = <div className={classes.Meat}></div>;
+            break;
+
+        case "cheese":
+            ingrediant = <div className={classes.Cheese}></div>;
+            break;
+
+        case "salad":
+            ingrediant = <div className={classes.Salad}></div>;
+            break;
+
+        case "bacon":
+            ingrediant = <div className={classes.Bacon}></div>;
+            break;
+
+        default:
+            ingrediant = null;
+    }
+
+    return ingrediant;
+};
+
+export default BurgerIngrediant;
